17d97302f4da143ad855e300f926e716d10a2ee8
Fernando Nielson <appsons.business@gmail.com>
2020-11-28 19:51:23 +0100

    y

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,20 @@ import { Injectable } from '@angular/core';
 })
 export class InfoPaginaService {
 
-  constructor() { }
+  info: any = {};
+  cargada = false;
+
+  constructor( private http: HttpClient ) { 
+
+    //console.log('Servicio de Pagina Listo GIL');
+
+    //Leo el archivo JSON
+    this.http.get('assets/data/data-pagina.json')
+        .subscribe( resp => {
+
+          this.cargada = true;
+          this.info = resp;
+          console.log(resp);
+        });
+  }
 }
